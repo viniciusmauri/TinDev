7d03266e9a17bafc4f36c7e94815973afb6f7ad6
Mauri Vinicius <maurimoura@ioasys.com.br>
2020-01-07 15:37:09 -0300

    Finalizando o Front e iniciando o Mobile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
@@ -7,6 +9,10 @@ routes.get('/', (req, res) => {
     return res.json({message: `Olá ${req.query.name}`});
 });
 
+routes.get('/devs', DevController.index);
+
 routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const server = express();
 mongoose.connect('mongodb+srv://mauri:M210818v@cluster0-hk10w.mongodb.net/omnistack8?retryWrites=true&w=majority',
     { useNewUrlParser:true, useUnifiedTopology: true
     });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3330);
